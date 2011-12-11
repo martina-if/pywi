1204156dc2fbc44275274aba8c3b930be4560f43
Martina Iglesias <martina.iglesiasf@gmail.com>
2011-12-11 19:45:25 +0100

    Forward navigation with sidebar (menu) working.

--- code.py
+++ code.py
@@ -4,9 +4,12 @@ import htmlize
 import os
 
 # TODO:
+# - Crear clase page o node, en vez de diccionarios. Factible usar
+#	objetos con templates fácilmente?
 # - os.path.join
+# - Meter funciones en clases
+# - Poner la mayor parte del html en templates
 
-#DATA_DIR = "/home/marta/software/sources/pywi/data/"
 DATA_DIR = "data/"
 EXTENSION = "txt"
 
@@ -15,8 +18,11 @@ urls = (
 		'/favicon.ico',		'favicon',
 		'/icons/(.*)',    	'static',
 		'/static/(.*)',   	'static',
-		'/(.*)',          	'page',
+		'/(.*)',          	'node',
 		)
+
+def delete_extension(filename):
+	return os.path.splitext(filename)[0]
 
 class favicon:
 	def GET(self):
@@ -24,59 +30,23 @@ class favicon:
 
 def is_page(filename, extension = EXTENSION):
 	try:
-		basename, ext = filename.split('.')
+		ext = filename.split('.')[-1]
 		return ext == extension
 	except ValueError:
 		return False
 
 
-#TODO TODO TODO 
 def get_menu(path):
-	#def get_submenu(path):
-		#data_dirs =  [d for d in os.listdir(path) 
-				#if os.path.isdir(os.path.join(path, d))]
-		#data_pages = [f for f in os.listdir(path) 
-				#if is_page(os.path.join(path, f))]
-		#pages = []
-		#for p in data_pages:
-			#pages = pages + [{'title':p, 'path':os.path.join(path,p)}]
-
-		#for i in data_dirs:
-			#pages = pages + [get_submenu(os.path.join(path,i))]
-		#return pages
-		
-	#data_pages = get_submenu(self.current_dir)
-	###########################################
 	# De momento sólo muestro un nivel de CATEGORÍAS
 	data_dirs = [{'title': os.path.basename(d), 'path': d} 
 				for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 	return data_dirs
 
-def get_menu_html(path):
-	path = os.path.dirname(path)
-	def render_item(item):
-		if type(item) is list:
-			try:
-				html = "<ul>\n" + render_item(item[0]) + "</ul>\n" + render_item(item[1:])
-			except IndexError:
-				html = "<ul>\n" + render_item(item[0]) + "</ul>\n"
-			return html
-		elif type(item) is dict:
-			return ("\t<li> <a href=\"" + item['path'] + "\"> "+ 
-				item['title'] + "</a></li>\n")
-		else: # No debería ocurrir
-			return ''
-	menu = get_menu(path)
-	print menu
-	html = '<div id=menu>\n' + render_item(menu) + "</div>"
-	return html
-
-	
 
 t_globals = {
 		'htmlize': htmlize.htmlize,
-		'get_menu_html': get_menu_html,
 }
+
 
 render = web.template.render('templates/', globals=t_globals)
 
@@ -85,21 +55,62 @@ class index:
 		# Esto es para coger el nombre del QS
 		#i = web.input(name=None)
 		#return render.index(i.name)
-		# Esco coge el nombre de la URL
+		# Esto coge el nombre de la URL
 		return render.index()
 
-class page:
-	def GET(self, page):
+class node:
+	def GET(self, page_url):
+		self.current_page = {}
+		self.current_page['url'] = str(page_url)
+		self.current_page['title'] = os.path.basename(str(page_url))
+		self.current_page['path'] = os.path.join(DATA_DIR, self.current_page['url'])
+		if os.path.isdir(self.current_page['path']):
 		#try:
-		if os.path.isdir(os.path.join(DATA_DIR, str(page))):
-			return "DIRECTORIO " + str(page)
+			self.current_dir = self.current_page['path']
+			print "CURRENT: ", self.current_page
+			return render.dir(self.current_page['path'], self)
 		else:
-			page_path = os.path.join(DATA_DIR, str(page) + "." + EXTENSION)
-			self.current_dir = os.path.dirname(page_path)
-			self.current_page = page_path
-			return render.page(page_path)
+			self.current_dir = os.path.dirname(self.current_page['path'])
+			self.current_page['path'] = os.path.join(self.current_page['path'] + 
+					"." + EXTENSION)
+			print "CURRENT: ", self.current_page
+			return render.page(self.current_page['path'], self)
 		#except:
 			#return render.notfound()
+
+
+	def get_menu_page(self):
+		# De momento sólo muestro un nivel de CATEGORÍAS
+		data_dirs = [
+					{'title': os.path.basename(d), 
+					#'url': os.path.join(os.path.dirname(self.current_page['url']), d),
+					'url': d,
+					'path': os.path.join(self.current_dir,d)} 
+					for d in os.listdir(self.current_dir)
+					if os.path.isdir(os.path.join(self.current_dir, d))
+					]
+		return render.menu(data_dirs)
+
+	def get_menu_dir(self):
+		# De momento sólo muestro un nivel de CATEGORÍAS
+		data_dirs = [
+					{'title': os.path.basename(d), 
+					'url': os.path.join("/", self.current_page['url'], d),
+					'path': os.path.join(self.current_dir,d)} 
+					for d in os.listdir(self.current_dir)
+					if os.path.isdir(os.path.join(self.current_dir, d))
+					]
+		return render.menu(data_dirs)
+
+	def get_pages_from_folder(self):
+		data_pages = [
+					{'title': delete_extension(f), 
+					'path': os.path.join(self.current_dir,f),
+					'url': delete_extension(os.path.join("/", self.current_page['url'], f)),
+					} for f in os.listdir(self.current_dir) 
+			if is_page(os.path.join(self.current_dir, f))]
+		return data_pages
+
 
 class static:
 	def GET(self, filename=None):
